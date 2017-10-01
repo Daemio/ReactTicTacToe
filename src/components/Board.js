4ca4b051c117449fceb28354d090abd5463f7b83
Demyan Zavgorodny <Daemio@users.noreply.github.com>
2017-10-01 14:32:15 +0300

    A bit of refactoring (#3)
    Now this seems to be a playable game
    
    This seems to be working

--- src/components/Board.js
+++ src/components/Board.js
@@ -19,15 +19,27 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.currentPlayer();
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext
     });
   }
 
+  currentPlayer() {
+    return this.state.xIsNext ? 'X' : 'O';
+  }
+
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    const status = winner ?
+      'Winner: ' + winner :
+      'Next Player: ' + this.currentPlayer();
 
     return (
       <div>
@@ -50,6 +62,26 @@ class Board extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
